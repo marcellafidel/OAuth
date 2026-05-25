ac9d5aade7e9e47ffdff47bb35239e2c1b71890e
marcellafidel <llagostudying@gmail.com>
2026-05-25 21:03:31 +0700

    fix syntax error

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL,,
+  callbackURL: process.env.CALLBACK_URL,
 }, (accessToken, refreshToken, profile, done) => {
   const user = {
     id: profile.id,
